7368a151e5a21eb4f8a6e5b74cc94e11f688a5d1
fx <fxinsky@163.com>
2022-10-03 21:12:17 +0800

    Initial commit

---
+++ src/test/java/com/fx/reggir_take_out/ReggirTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fx.reggir_take_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggirTakeOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

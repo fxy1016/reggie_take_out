5efe41b6341fd6555cb5e3f9c2be5a09adad5923
fx <fxinsky@163.com>
2022-10-04 13:00:04 +0800

    完善登录功能，增加拦截器，判断是否需要拦截，是否登录

--- src/main/java/com/fx/reggie_take_out/ReggieTakeOutApplication.java
+++ src/main/java/com/fx/reggie_take_out/ReggieTakeOutApplication.java
@@ -3,9 +3,12 @@ package com.fx.reggie_take_out;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieTakeOutApplication {
 
 	public static void main(String[] args) {
